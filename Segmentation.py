38bfb5c57e186518bf6b8605453dcfdd1fcd827e
99Abdel <a.msaad@studenti.unibs.it>
2022-11-08 17:26:16 +0100

    segmentation finish

--- Segmentation.py
+++ Segmentation.py
@@ -1,251 +1,94 @@
-import cap
-import cv2, pygame as pg
+import cv2
+import numpy as np
+import pygame as pg
 from tshirt import *
 
-# Capturing video through webcam
-webcam = cv2.VideoCapture(0)
-
-color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
-                  'white': [[180, 18, 255], [0, 0, 231]],
-                  'red1': [[180, 255, 255], [159, 50, 70]],
-                  'red2': [[9, 255, 255], [0, 50, 70]],
-                  'green': [[89, 255, 255], [36, 50, 70]],
-                  'blue': [[128, 255, 255], [90, 50, 70]],
-                  'yellow': [[35, 255, 255], [25, 50, 70]],
-                  'purple': [[158, 255, 255], [129, 50, 70]],
-                  'orange': [[24, 255, 255], [10, 50, 70]],
-                  'gray': [[180, 18, 230], [0, 0, 40]]}
-
-# attempt to track number of colors in the image
-nb_colors = 0
+# image constant utilities
+RESOLUTION = 640*480
+UP_LIM = RESOLUTION*0.3
+LOW_LIM = RESOLUTION*0.01
 
 # initialization of clock to measure time
+SETTLING_TIME = 2000
 pg.init()
-
 clk = pg.time.Clock()
 time = 0
 
-
-# color detection
-
-def color_dtct(a):
-    # [red,Blue,purple,green,yellow]
-
-    if a[0]:
-        return "Red"
-    if a[1]:
-        return "Blue"
-    if a[2]:
-        return "Purple"
-    if a[3]:
-        return "Green"
-    if a[4]:
-        return "Yellow"
+# open cv initialization
+cap = cv2.VideoCapture(0)
+kernel = np.ones((10, 10), np.uint8)
 
 
-def doSegmentation():
+def color_frame_morpho(hsv_frame,frame,low,high,kernel):
 
-    while True:
-        clk.tick()
-        # Reading the video from the
-        # webcam in image frames
-        _, imageFrame = webcam.read()
+    # mask
+    color_mask = cv2.inRange(hsv_frame, low, high)
+    color = cv2.bitwise_and(frame, frame, mask=color_mask)
 
-        # Convert the imageFrame in
-        # BGR(RGB color space) to
-        # HSV(hue-saturation-value)
-        # color space
-        hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
-        rgbFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2RGB)
-        # brightness = np.sqrt(0.241*pow(rgbFrame[0, :, 0],2) + 0.691*pow(rgbFrame[0, :, 1],2) + 0.068*pow(rgbFrame[0, :, 2],2))
+    # mask after morphology
+    color_mask_closing = cv2.morphologyEx(color_mask, cv2.MORPH_CLOSE, kernel)
+    color_closed = cv2.bitwise_and(frame, frame, mask=color_mask_closing)
 
-        # Set range for red color 1 and
-        # define mask
-        red1_lower = np.array(color_dict_HSV['red1'][1], np.uint8)
-        red1_upper = np.array(color_dict_HSV['red1'][0], np.uint8)
-        red1_mask = cv2.inRange(hsvFrame, red1_lower, red1_upper)
+    color_closed_npixel = np.count_nonzero(color_mask_closing)
 
-        red2_lower = np.array(color_dict_HSV['red2'][1], np.uint8)
-        red2_upper = np.array(color_dict_HSV['red2'][0], np.uint8)
-        red2_mask = cv2.inRange(hsvFrame, red2_lower, red2_upper)
+    return color,color_closed,color_closed_npixel
 
-        red_mask = red1_mask + red2_mask
 
-        # Set range for green color and
-        # define mask
-        green_lower = np.array(color_dict_HSV['green'][1], np.uint8)
-        green_upper = np.array(color_dict_HSV['green'][0], np.uint8)
-        green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+def show_frames(frame,color1_closed,color2_closed,f_name,c1_name,c2_name):
 
-        # Set range for blue color and
-        # define mask
-        blue_lower = np.array(color_dict_HSV['blue'][1], np.uint8)
-        blue_upper = np.array(color_dict_HSV['blue'][0], np.uint8)
-        blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+    cv2.imshow(f_name, frame)
+    cv2.imshow(c1_name, red_closed)
+    cv2.imshow(c2_name, blue_closed)
 
-        # Set range for yellow color and
-        # define mask
-        yellow_lower = np.array(color_dict_HSV['yellow'][1], np.uint8)
-        yellow_upper = np.array(color_dict_HSV['yellow'][0], np.uint8)
-        yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper)
 
-        # Set range for purple color and
-        # define mask
-        purple_lower = np.array(color_dict_HSV['purple'][1], np.uint8)
-        purple_upper = np.array(color_dict_HSV['purple'][0], np.uint8)
-        purple_mask = cv2.inRange(hsvFrame, purple_lower, purple_upper)
+while True:
+    clk.tick()
+    _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Morphological Transform, Dilation
-        # for each color and bitwise_and operator
-        # between imageFrame and mask determines
-        # to detect only that particular color
-        kernal = np.ones((5, 5), "uint8")
+    color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
+                      'white': [[180, 18, 255], [0, 0, 231]],
+                      'red1': [[180, 255, 255], [159, 50, 70]],
+                      'red2': [[9, 255, 255], [0, 50, 70]],
+                      'green': [[89, 255, 255], [36, 50, 70]],
+                      'blue': [[128, 255, 255], [90, 50, 70]],
+                      'yellow': [[35, 255, 255], [25, 50, 70]],
+                      'purple': [[158, 255, 255], [129, 50, 70]],
+                      'orange': [[24, 255, 255], [10, 50, 70]],
+                      'gray': [[180, 18, 230], [0, 0, 40]]}
 
-        # For red color
-        red_mask1 = cv2.dilate(red1_mask, kernal)
-        res_red1 = cv2.bitwise_and(imageFrame, imageFrame,
-                                   mask=red_mask1)
+    # Red color
+    low_red = np.array([161, 155, 84])
+    high_red = np.array([179, 255, 255])
 
-        red_mask2 = cv2.dilate(red2_mask, kernal)
-        res_red2 = cv2.bitwise_and(imageFrame, imageFrame,
-                                   mask=red_mask2)
+    # Blue color
+    low_blue = np.array([94, 80, 2])
+    high_blue = np.array([126, 255, 255])
 
-        # For green color
-        green_mask = cv2.dilate(green_mask, kernal)
-        res_green = cv2.bitwise_and(imageFrame, imageFrame,
-                                    mask=green_mask)
+    [red,red_closed,red_closed_npixel] = color_frame_morpho(hsv_frame,frame,low_red,high_red,kernel)
+    [blue, blue_closed, blue_closed_npixel] = color_frame_morpho(hsv_frame, frame, low_blue, high_blue, kernel)
 
-        # For blue color
-        blue_mask = cv2.dilate(blue_mask, kernal)
-        res_blue = cv2.bitwise_and(imageFrame, imageFrame,
-                                   mask=blue_mask)
+    show_frames(frame,red_closed,blue_closed,"Frame","Red","Blue")
 
-        # For yellow color
-        yellow_mask = cv2.dilate(yellow_mask, kernal)
-        res_yellow = cv2.bitwise_and(imageFrame, imageFrame,
-                                     mask=yellow_mask)
+    if(red_closed_npixel > UP_LIM and blue_closed_npixel < LOW_LIM):
+        time += clk.get_time()
+        c = "RED"
+    elif(blue_closed_npixel > UP_LIM and red_closed_npixel < LOW_LIM):
+        time += clk.get_time()
+        c = "BLUE"
+    else:
+        time = 0
+        c = ""
 
-        # For purple color
-        purple_mask = cv2.dilate(purple_mask, kernal)
-        res_purple = cv2.bitwise_and(imageFrame, imageFrame,
-                                     mask=purple_mask)
+    #print(time)
 
-        # Creating contour to track red color
-        contours, hierarchy = cv2.findContours(red_mask,
-                                               cv2.RETR_TREE,
-                                               cv2.CHAIN_APPROX_SIMPLE)
+    if c != "" and time >= SETTLING_TIME:
+        # DO A MEAN OF THE HSV_FRAME COLUMNS BEFORE CREATING A TSHIRT CLASS.
+        tshirt = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2], c)
+        print(c)
 
-        red = 0
-        Blue = 0
-        green = 0
-        purple = 0
-        yellow = 0
-        detectable_area = 30000
 
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if (area > detectable_area):
-                red = 1
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (0, 0, 255), 2)
+    key = cv2.waitKey(1)
 
-                cv2.putText(imageFrame, "Red Colour", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                            (0, 0, 255))
-
-        # Creating contour to track green color
-        contours, hierarchy = cv2.findContours(green_mask,
-                                               cv2.RETR_TREE,
-                                               cv2.CHAIN_APPROX_SIMPLE)
-
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if (area > detectable_area):
-                green = 1
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (0, 255, 0), 2)
-
-                cv2.putText(imageFrame, "Green Colour", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (0, 255, 0))
-
-        # Creating contour to track blue color
-        contours, hierarchy = cv2.findContours(blue_mask,
-                                               cv2.RETR_TREE,
-                                               cv2.CHAIN_APPROX_SIMPLE)
-
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if (area > detectable_area):
-                Blue = 1
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (255, 0, 0), 2)
-
-                cv2.putText(imageFrame, "Blue Colour", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (255, 0, 0))
-
-        # Creating contour to track yellow color
-        contours, hierarchy = cv2.findContours(yellow_mask,
-                                               cv2.RETR_TREE,
-                                               cv2.CHAIN_APPROX_SIMPLE)
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if (area > detectable_area):
-                yellow = 1
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (0, 255, 255), 2)
-
-                cv2.putText(imageFrame, "Yellow Colour", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (0, 255, 255))
-
-        # Creating contour to track purple color
-        contours, hierarchy = cv2.findContours(purple_mask,
-                                               cv2.RETR_TREE,
-                                               cv2.CHAIN_APPROX_SIMPLE)
-        for pic, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if (area > detectable_area):
-                purple = 1
-                x, y, w, h = cv2.boundingRect(contour)
-                imageFrame = cv2.rectangle(imageFrame, (x, y),
-                                           (x + w, y + h),
-                                           (255, 0, 255), 2)
-
-                cv2.putText(imageFrame, "Purple Colour", (x, y),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (255, 0, 255))
-
-        nb_colors = red + Blue + purple + green + yellow
-        colors = [red, Blue, purple, green, yellow]
-
-        single_color = (1 if nb_colors == 1 else 0)
-        # print(nb_colors)
-        if single_color:
-            time += clk.get_time()
-        elif not single_color:
-            time = 0
-        print(time)
-        if single_color and time >= 2000:
-            thshirt = Tshirt(hsvFrame[0, -1, 0], hsvFrame[0, -1, 1], hsvFrame[0, -1, 2], color_dtct(colors))
-            print(color_dtct(colors))
-
-        # Program Termination
-        cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            cap.release()
-            cv2.destroyAllWindows()
-            break
-
-        key = cv2.waitKey(1)
-        if key == 27:  # Esc
-            break
+    if key == 27:
+        break
