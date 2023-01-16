5ed8dd630d79c4158c664d9e2f95d3522f0d3637
Marco Milanesi <marco.milanesi.99@gmail.com>
2023-01-16 22:03:12 +0100

    background removal fixed

--- Segmentation.py
+++ Segmentation.py
@@ -3,22 +3,7 @@ import numpy as np
 import pygame as pg
 from tshirt import *
 
-# image constant utilities
-#RESOLUTION = 640 * 480
-RESOLUTION = 1920 * 1080
-UP_LIM = RESOLUTION * 0.3
-LOW_LIM = RESOLUTION * 0.01
-
-# initialization of clock to measure time
-SETTLING_TIME = 2000
-pg.init()
-clk = pg.time.Clock()
-time = 0
-
-# open cv initialization
-cap = cv2.VideoCapture(0)
 kernel = np.ones((10, 10), np.uint8)
-
 
 def color_frame_morpho(hsv_frame, frame, low, high, kernel):
     # mask
@@ -40,25 +25,11 @@ def show_frames(frame, color1_closed, color2_closed, f_name, c1_name, c2_name):
     cv2.imshow(c2_name, color2_closed)
 
 
-while True:
-    clk.tick()
-    _, frame = cap.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
-                      'white': [[180, 18, 255], [0, 0, 231]],
-                      'red1': [[180, 255, 255], [159, 50, 70]],
-                      'red2': [[9, 255, 255], [0, 50, 70]],
-                      'green': [[89, 255, 255], [36, 50, 70]],
-                      'blue': [[128, 255, 255], [90, 50, 70]],
-                      'yellow': [[35, 255, 255], [25, 50, 70]],
-                      'purple': [[158, 255, 255], [129, 50, 70]],
-                      'orange': [[24, 255, 255], [10, 50, 70]],
-                      'gray': [[180, 18, 230], [0, 0, 40]]}
+def segmentation(hsv_frame, frame):
 
     # Red color
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
+    low_red = np.array([5, 50, 70])
+    high_red = np.array([50, 255, 255])
 
     # Blue color
     low_blue = np.array([94, 80, 2])
@@ -67,26 +38,10 @@ while True:
     [red, red_closed, red_closed_npixel] = color_frame_morpho(hsv_frame, frame, low_red, high_red, kernel)
     [blue, blue_closed, blue_closed_npixel] = color_frame_morpho(hsv_frame, frame, low_blue, high_blue, kernel)
 
-    show_frames(frame, red_closed, blue_closed, "Frame", "Red", "Blue")
+    #show_frames(frame, red_closed, blue_closed, "Frame", "Red", "Blue")
 
-    if (red_closed_npixel > UP_LIM and blue_closed_npixel < LOW_LIM):
-        time += clk.get_time()
-        c = "RED"
-    elif (blue_closed_npixel > UP_LIM and red_closed_npixel < LOW_LIM):
-        time += clk.get_time()
-        c = "BLUE"
-    else:
-        time = 0
-        c = ""
+    orange_percentage = np.sum(red_closed) / np.size(red_closed)
+    blue_percentage = np.sum(blue_closed) / np.size(blue_closed)
 
-    # print(time)
+    return orange_percentage, blue_percentage
 
-    if c != "" and time >= SETTLING_TIME:
-        # DO A MEAN OF THE HSV_FRAME COLUMNS BEFORE CREATING A TSHIRT CLASS.
-        tshirt = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2], c)
-        print(c)
-
-    key = cv2.waitKey(1)
-
-    if key == 27:
-        break
--- background_removal.py
+++ background_removal.py
@@ -4,27 +4,8 @@ from tshirt import Tshirt
 from skimage import morphology
 import time
 from audio import make_sentence
-
-
-def color_frame_morpho(hsv_frame, frame, low, high, kernel):
-    # mask
-    color_mask = cv2.inRange(hsv_frame, low, high)
-    color = cv2.bitwise_and(frame, frame, mask=color_mask)
-
-    # mask after morphology
-    color_mask_closing = cv2.morphologyEx(color_mask, cv2.MORPH_CLOSE, kernel)
-    color_closed = cv2.bitwise_and(frame, frame, mask=color_mask_closing)
-
-    color_closed_npixel = np.count_nonzero(color_mask_closing)
-
-    return color, color_closed, color_closed_npixel
-
-
-def show_frames(frame, color1_closed, color2_closed, f_name, c1_name, c2_name):
-    cv2.imshow(f_name, frame)
-    cv2.imshow(c1_name, color1_closed)
-    cv2.imshow(c2_name, color2_closed)
-
+from Segmentation import segmentation
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
                   'white': [[180, 18, 255], [0, 0, 231]],
@@ -41,8 +22,8 @@ RESOLUTION = 1920 * 1080
 UP_LIM = RESOLUTION * 0.3
 LOW_LIM = RESOLUTION * 0.01
 
-N_FRAME = 10
-FRAME_TOLL = 2
+N_FRAME = 5
+FRAME_TOLL = 5
 WHITE_THRESHOLD = 20
 
 WAIT_TIME = 8
@@ -54,7 +35,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 # fontScale
 fontScale = 1
 # Blue color in BGR
-color = (0, 0, 0)
+color = (255, 255, 255)
 # Line thickness of 2 px
 thickness = 2
 flag_first_tshirt = True
@@ -64,33 +45,13 @@ time_seconds = 0
 list_tshirt_group1 = []
 list_tshirt_group2 = []
 
-# Open Video
-cap = cv2.VideoCapture('video_test/unsorted.mp4')
-
 # Open Camera
-#cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-#
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-# Randomly select 25 frames
-frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
+segmentor = SelfiSegmentation()
 
-# Store selected frames in an array
-frames = []
-for fid in frameIds:
-    cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
-    ret, frame = cap.read()
-    frames.append(frame)
-
-# Calculate the median along the time axis
-medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
-
-# Reset frame number to 0
-cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-# Convert background to grayscale
-grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
 white_pixel_percentage = 0
 count_first_frame_above_threshold = 0  # number of consecutive frames above the 20% threshold
 count_similar_frames = 0  # number of equal consecutive frames
@@ -103,6 +64,8 @@ original_tshirt_dictionary = dict()
 new_tshirt_dictionary = dict()
 start_segmentation_text = ''
 isBackgroudRemovalFineshed = False
+orange_percentage = 0
+blue_percentage = 0
 
 start = time.time()
 # Loop over all frames
@@ -115,14 +78,10 @@ while (cap.isOpened()):
         hsv_frame = cv2.cvtColor(frame_color, cv2.COLOR_BGR2HSV)
         # Convert current frame to grayscale
         frame = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
-        # Calculate absolute difference of current frame and
-        # the median frame
 
-        dframe = cv2.absdiff(frame, grayMedianFrame)
-        # Treshold to binarize
-        th, dframe = cv2.threshold(dframe, 60, 255, cv2.THRESH_BINARY)
+        BW_frame = segmentor.removeBG(frame_color, (0, 0, 0), threshold=0.7)
         kernel = np.ones((10, 10), "uint8")
-        erosion = cv2.erode(dframe, kernel, iterations=1)
+        erosion = cv2.erode(BW_frame, kernel, iterations=1)
         cleaned = morphology.remove_small_objects(erosion, min_size=150, connectivity=150)
         white_pixel_percentage = np.sum(cleaned) / np.size(cleaned)
 
@@ -134,25 +93,13 @@ while (cap.isOpened()):
                     start_segmentation_text = 'F'
                     # time.sleep(0.5)
                     start = time.time()
-                    # Red color
-                    low_orange = np.array([5, 50, 70])
-                    high_orange = np.array([50, 255, 255])
 
-                    # Blue color
-                    low_blue = np.array([94, 80, 2])
-                    high_blue = np.array([126, 255, 255])
+                    [orange_percentage, blue_percentage] = segmentation(hsv_frame, cleaned)
 
-                    [red, red_closed, red_closed_npixel] = color_frame_morpho(hsv_frame, frame_color, low_orange,
-                                                                              high_orange, kernel)
-                    [blue, blue_closed, blue_closed_npixel] = color_frame_morpho(hsv_frame, frame_color, low_blue,
-                                                                                 high_blue, kernel)
-                    # uncomment to see the different color in a window
-                    # show_frames(frame, red_closed, blue_closed, "Frame", "Orange", "Blue")
-
-                    if (red_closed_npixel > UP_LIM and blue_closed_npixel < LOW_LIM):
+                    if orange_percentage > 40 and blue_percentage < 10:
                         orange_tshirt_number += 1
                         color_name = 'ORANGE' + str(orange_tshirt_number)
-                    elif (blue_closed_npixel > UP_LIM and red_closed_npixel < LOW_LIM):
+                    elif blue_percentage > 40 and orange_percentage < 10:
                         blue_tshirt_number += 1
                         color_name = 'BLUE' + str(blue_tshirt_number)
                     else:
@@ -161,11 +108,9 @@ while (cap.isOpened()):
                     if color_name != 'None':
                         # DO A MEAN OF THE HSV_FRAME COLUMNS BEFORE CREATING A TSHIRT CLASS.
                         if 'BLUE' in color_name:
-                            tshirt_temp = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2],
-                                                 color_name, 0)
+                            tshirt_temp = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2], color_name, 0)
                         elif 'ORANGE' in color_name:
-                            tshirt_temp = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2],
-                                                 color_name, 1)
+                            tshirt_temp = Tshirt(hsv_frame[0, -1, 0], hsv_frame[0, -1, 1], hsv_frame[0, -1, 2], color_name, 1)
 
                         if color_name not in original_tshirt_dictionary.keys():
                             original_tshirt_dictionary[color_name] = tshirt_temp.brightness
@@ -219,11 +164,13 @@ while (cap.isOpened()):
             time_seconds = 0
 
         time_seconds = end - start
-        frame_color = cv2.putText(frame_color, ('W/B Perc. : %.2f' % white_pixel_percentage), (900, 50), font,
+        cleaned = cv2.putText(cleaned, ('W/B Perc. : %.2f' % white_pixel_percentage), (900, 50), font,
                                   fontScale, color, thickness, cv2.LINE_AA)
-        frame_color = cv2.putText(frame_color, ('Time: %.2f' % time_seconds), (900, 80), font, fontScale, color,
+        cleaned = cv2.putText(cleaned, ('Time: %.2f' % time_seconds), (900, 80), font, fontScale, color,
                                   thickness, cv2.LINE_AA)
-        frame_color = cv2.putText(frame_color, start_segmentation_text, (1600, 110), font, 4, color, 3, cv2.LINE_AA)
+        cleaned = cv2.putText(cleaned, ('Blue: %.2f' % blue_percentage + 'Orange: %.2f' % orange_percentage), (900, 110), font, fontScale, color,
+                                  thickness, cv2.LINE_AA)
+        cleaned = cv2.putText(cleaned, start_segmentation_text, (1600, 110), font, 4, color, 3, cv2.LINE_AA)
         new_tshirt_dictionary = original_tshirt_dictionary.copy()
 
         if time_seconds > 0.5:
@@ -236,17 +183,17 @@ while (cap.isOpened()):
             org = (50, y_pos1)
             text_to_print = 'Color Recognized: ' + x
             # Using cv2.putText() method
-            frame_color = cv2.putText(frame_color, text_to_print, org, font, fontScale, color, thickness, cv2.LINE_AA)
+            cleaned = cv2.putText(cleaned, text_to_print, org, font, fontScale, color, thickness, cv2.LINE_AA)
 
             y_pos2 = 80 + 60 * index
             org = (50, y_pos2)
             text_to_print = 'Brightness: ' + ('%.2f' % original_tshirt_dictionary[x])
-            frame_color = cv2.putText(frame_color, text_to_print, org, font, fontScale, color, thickness, cv2.LINE_AA)
+            cleaned = cv2.putText(cleaned, text_to_print, org, font, fontScale, color, thickness, cv2.LINE_AA)
 
         # Display image
-        cv2.imshow('frame2', frame_color)
+        cv2.imshow('frame2', cleaned)
         n_pixel = 0
-        key = cv2.waitKey(10)
+        key = cv2.waitKey(50)
         if key == ord('q'):
             break
     else:
